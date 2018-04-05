0474377d1d996954d8d5014426f1523f5040ddae
Preums <preums@gmx.com>
2018-04-05 16:47:20 +0200

    some change for app.js including the public directory use

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ let app = express();
 let path = require("path");
 
 let index = require('./routes/index');
-let about = require('./routes/about');
-let coucou = require('./routes/Coucou');
-let bonjour = require('./routes/Bonjour');
+let index2 = require('./routes/index2');
+let index3 = require('./routes/index3');
+
 
 app.use('/', index);
-app.use('/about', about);
-app.use('/coucou', coucou);
-app.use('/bonjour', bonjour);
+app.use('/index2', index2);
+app.use('/index3', index3);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.listen(3000);
 
 module.exports = app;

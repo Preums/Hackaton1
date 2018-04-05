bebe2b24a979b604e6376a459b51164db5f33de6
githubfc <profchene@gmail.com>
2018-04-05 22:38:20 +0200

    dynamic all fighter select

--- public/javascript/choosePlayer.js
+++ public/javascript/choosePlayer.js
@@ -1,19 +1,25 @@
  let selected = 0;
-
- document.getElementById("f01").addEventListener("click", function( event ) {
- 	let url = document.getElementById("f01").src;
- 	document.getElementById("fighter_01").src= url;
- 	selected +=1;
- 	apply();
- });
+ let fightersP1 = document.getElementsByClassName("forP1");
+ let fightersP2 = document.getElementsByClassName("forP2");
 
 
-  document.getElementById("f05").addEventListener("click", function( event ) {
- 	let url = document.getElementById("f05").src;
- 	document.getElementById("fighter_02").src= url;
- 	selected +=1;
- 	apply();
- });
+ for (let i = 0; i < fightersP1.length; i++) {
+ 	fightersP1[i].addEventListener("click", function( event ) {
+ 		let url = fightersP1[i].src;
+ 		document.getElementById("fighter_01").src= url;
+ 		selected +=1;
+ 		apply();
+ 	});
+ };
+
+ for (let i = 0; i < fightersP2.length; i++) {
+ 	fightersP2[i].addEventListener("click", function( event ) {
+ 		let url = fightersP2[i].src;
+ 		document.getElementById("fighter_02").src= url;
+ 		selected +=1;
+ 		apply();
+ 	});
+ };
 
 
 const apply = ()=>{

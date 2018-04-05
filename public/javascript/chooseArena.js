264c45e1362ec28c0482527d9c2da0148f596f6a
Estelle Doreau <doreau.e@gmail.com>
2018-04-05 22:06:25 +0200

    opti js choose arena

--- public/javascript/chooseArena.js
+++ public/javascript/chooseArena.js
@@ -1,32 +1,11 @@
- document.getElementById("background_01").addEventListener("click", function( event ) {
- 	let url = document.getElementById("background_01").src;
- 	document.getElementById("Fight").style.display = 'flex';
- 	document.getElementById("Fight").style.backgroundImage = 'url(' + url + ')';
- 	document.getElementById("Choose_arena").style.display = 'none';
- 	document.getElementById("Choose_fighter").style.display = 'block';
- });
+let backgrounds = document.getElementsByClassName("arena");
 
-  document.getElementById("background_02").addEventListener("click", function( event ) {
- 	let url = document.getElementById("background_02").src;
+for (let i = 0; i < backgrounds.length; i++) {
+	backgrounds[i].addEventListener("click", function( event ) {
+ 	let url = backgrounds[i].src;
  	document.getElementById("Fight").style.display = 'flex';
  	document.getElementById("Fight").style.backgroundImage = 'url(' + url + ')';
  	document.getElementById("Choose_arena").style.display = 'none';
  	document.getElementById("Choose_fighter").style.display = 'block';
  });
-
-  document.getElementById("background_03").addEventListener("click", function( event ) {
- 	let url = document.getElementById("background_03").src;
- 	document.getElementById("Fight").style.display = 'flex';
- 	document.getElementById("Fight").style.backgroundImage = 'url(' + url + ')';
- 	document.getElementById("Choose_arena").style.display = 'none';
- 	document.getElementById("Choose_fighter").style.display = 'block';
- });
-
-  document.getElementById("background_04").addEventListener("click", function( event ) {
- 	let url = document.getElementById("background_04").src;
- 	document.getElementById("Fight").style.display = 'flex';
- 	document.getElementById("Fight").style.backgroundImage = 'url(' + url + ')';
- 	document.getElementById("Choose_arena").style.display = 'none';
- 	document.getElementById("Choose_fighter").style.display = 'block';
- });
-  
+}

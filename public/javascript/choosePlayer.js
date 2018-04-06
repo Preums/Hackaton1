cf1b582ae720d92b0985534f236804817a81d981
Preums <preums@gmx.com>
2018-04-06 13:20:49 +0200

    first usable version

--- public/javascript/choosePlayer.js
+++ public/javascript/choosePlayer.js
@@ -1,75 +1,24 @@
-let selected = 0;
-let fightersP1 = document.getElementsByClassName("forP1");
-let fightersP2 = document.getElementsByClassName("forP2");
-
-let fighter1Id = Math.ceil(Math.random()*731);
-let fighter2Id = Math.ceil(Math.random()*731);
-
-let rand;
-let var1;
-let var2;
-
-let player1;
-let player2;
-
-window.addEventListener('load', function() {
-	fetch(`https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json`)
-	.then(function(response) {
-		let result = response.json();
-		return result
-	})
-	.then(function(myJson) {
-		for (var i = 0; i < 4; i++) {
-			rand = Math.round(Math.random()*myJson.length);
-			var1 = myJson[rand];
-			fightersP1[i].src=var1.images.xs;
-      let src = fightersP1[i].src
-			fightersP1[i].addEventListener("click", function(event) {
-				event.preventDefault();
-				document.getElementById('fighter_01').src= src;
-				player1 = new player(var1.name, var1.powerstats.power, var1.powerstats.speed, var1.powerstats.strength, var1.powerstats.intelligence, var1.powerstats.durability);
-				selected +=1;
- 				apply();
-			})
+class player {
+	constructor (name, speed, power, strength, intelligence,durability) {
+		this.name = name;
+		this.speed = speed;
+		this.power = power;
+		this.strength = strength;
+		this.power = power;
+		this.intelligence = intelligence;
+		this.durability = durability;
 		}
-
-		for (var j = 0; j < 4; j++) {
-			rand = Math.round(Math.random()*myJson.length);
-			var2 = myJson[rand];
-			fightersP2[j].src=var2.images.xs;
-      let src = fightersP2[i].src
-			fightersP2[j].addEventListener("click", function(event) {
-				event.preventDefault();
-				document.getElementById('fighter_02').src=src;
-				player2 = new player(var2.name, var2.powerstats.power, var2.powerstats.speed, var2.powerstats.strength, var2.powerstats.intelligence, var2.powerstats.durability);
-				selected +=1;
- 				apply();
-			})
-		}
-	});	
-})
-
-
-
-const apply = ()=>{
-	if (selected === 2){
-	document.getElementsByClassName("toggle")[0].style.display = 'none';
-	document.getElementsByClassName("toggle")[1].style.display = 'none';
-  	document.getElementById("Choose").style.display = 'none';
-	document.getElementById("Start_fight").style.display = 'block';
-	document.getElementById("gamePad").style.display = 'flex';
-  };
-};
+}
 
 class combat {
 	constructor(player1,player2) {
-		this.fistP1 = player1.speed + player1.power;
-		this.kickP1 = player1.strength + player1.power;
-		this.dodgeP1 = player1.intelligence + player1.speed;
+		this.fistP1 = (player1.speed + player1.power)/2;
+		this.kickP1 = (player1.strength + player1.power)/2;
+		this.dodgeP1 = (player1.intelligence + player1.speed)/2;
 
-		this.fistP2 = player2.speed + player1.power;
-		this.kickP2 = player2.strength + player1.power;
-		this.dodgeP2 = player2.intelligence + player1.speed;
+		this.fistP2 = (player2.speed + player1.power)/2;
+		this.kickP2 = (player2.strength + player1.power)/2;
+		this.dodgeP2 = (player2.intelligence + player1.speed)/2;
 	}
 
 	getRound (player1,player2,coup1,coup2) {
@@ -79,9 +28,9 @@ class combat {
 			let degatP2 = Math.random()*this.fistP1;
 			player2.durability -= degatP2;
 			if (degatP1>degatP2) {
-				return `le poing de ${player1.nameP1} est plus incrusté dans la joue de ${player2.name} que l'inverse;`
+				return `le poing de ${player1.name} est plus incrusté dans la joue de ${player2.name} que l'inverse;`
 			}
-			return `le poing de ${player2.nameP2} est plus incrusté dans la joue de ${player1.name} que l'inverse`;
+			return `le poing de ${player2.name} est plus incrusté dans la joue de ${player1.name} que l'inverse`;
 
 		} else if ((coup1 ===2) && (coup2 ===2)) {
 			let degatP1 = Math.random()*this.kickP2;
@@ -176,21 +125,105 @@ class combat {
 
 }
 
+let selected = 0;
+let fightersP1 = document.getElementsByClassName("forP1");
+let fightersP2 = document.getElementsByClassName("forP2");
+let button_fight_start = document.querySelector("#Start_fight");
 
-class player {
-	constructor (name, speed, power, strength, intelligence,durability) {
-		this.name = name;
-		this.speed = speed;
-		this.power = power;
-		this.strength = strength;
-		this.power = power;
-		this.intelligence = intelligence;
-		this.durability = durability;
-		}
-}
+let fighter1Id = Math.ceil(Math.random()*731);
+let fighter2Id = Math.ceil(Math.random()*731);
 
-let player1 = new player();
+let rand;
+let var1;
+let var2;
+
+let player1;
 let player2;
+
+window.addEventListener('load', function() {
+
+	fetch(`https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json`)
+	.then(function(response) {
+		let result = response.json();
+		return result
+	})
+	.then(function(myJson) {
+		for (var i = 0; i < 4; i++) {
+			rand = Math.round(Math.random()*myJson.length);
+			var1 = myJson[rand];
+			fightersP1[i].src = var1.images.xs;
+      		let src = myJson[rand];
+			fightersP1[i].addEventListener("click", function(event) {
+				event.preventDefault();
+				document.getElementById('fighter_01').src= src.images.sm;
+				player1 = new player(src.name, src.powerstats.power, src.powerstats.speed, src.powerstats.strength, src.powerstats.intelligence, src.powerstats.durability);
+				selected +=1;
+ 				apply();
+			})
+		}
+
+		for (var j = 0; j < 4; j++) {
+			rand = Math.round(Math.random()*myJson.length);
+			var2 = myJson[rand];
+			fightersP2[j].src = var2.images.xs;
+      		let src = myJson[rand];
+			fightersP2[j].addEventListener("click", function(event) {
+				event.preventDefault();
+				document.getElementById('fighter_02').src=src.images.sm;
+				player2 = new player(src.name, src.powerstats.power, src.powerstats.speed, src.powerstats.strength, src.powerstats.intelligence, src.powerstats.durability);
+				selected +=1;
+ 				apply();
+			})
+		}
+
+	});
+// setTimeout(function() {
+		// console.log("player1 :", player1.name);
+		// let newFight = new combat(player1,player2);
+		// console.log(getRound(player1,player2,1,1));
+		// console.log(getWinner(player1,player2));
+		
+// 	 }, 5000)
+
+})
+
+const apply = ()=>{
+	if (selected === 2){
+	document.getElementsByClassName("toggle")[0].style.display = 'none';
+	document.getElementsByClassName("toggle")[1].style.display = 'none';
+  	document.getElementById("Choose").style.display = 'none';
+	document.getElementById("Start_fight").style.display = 'block';
+	document.getElementById("gamePad").style.display = 'flex';
+  };
+};
+
+
+
+button_fight_start.addEventListener("click", function() {
+		let newFight = new combat(player1,player2);
+
+		let roundResult = document.createElement('p');
+		roundResult.textContent=newFight.getRound(player1,player2,1,1);
+		roundResult.style.backgroundColor='white';
+
+		document.getElementById('fight_btn').appendChild(roundResult);
+		document.getElementById("Start_fight").textContent = newFight.getWinner(player1,player2);
+})
+
+
+
+// class player {
+// 	constructor (name, speed, power, strength, intelligence,durability) {
+// 		this.name = name;
+// 		this.speed = speed;
+// 		this.power = power;
+// 		this.strength = strength;
+// 		this.power = power;
+// 		this.intelligence = intelligence;
+// 		this.durability = durability;
+// 		}
+// }
+
 
 // function createPlayer1() {
 // 	fetch(`https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/id/5.json`)
@@ -208,12 +241,10 @@ let player2;
 // 	});
 // }
 
-createPlayer1()
-createPlayer2()
+// createPlayer1()
+// createPlayer2()
 
-setTimeout(function() {
-	console.log("war1 : ", player1);
-	console.log("war2 : ", player2);
-	console.log("intelligence war1 : ", player4.intelligence);
-	console.log("speed war2 : ", player2.speed);
-}, 2000)
+// setTimeout(function() {
+// 	console.log("war1 : ", player1);""
+//  	console.log("war2 : ", player2);
+//  }, 5000)
